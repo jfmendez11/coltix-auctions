d8092df9b36910b1dcf633835fbe4fd19d869639
jfmendez11 <jf.mendez11@uniandes.edu.co>
2018-03-28 18:14:39 -0500

    Ready to go

---
+++ imports/ui/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+
+export default class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+};
+
+App.propTypes = {
+
+};
